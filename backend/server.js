390110ca0418900c5651643791c49afcecd7acda
theman-ojjas <123209996+theman-ojjas@users.noreply.github.com>
2024-12-21 03:38:14 +0530

    Update server2.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://real-patti-1.onrender.com/",
+        origin: "https://real-patti-1.onrender.com",
       },
     });
 
